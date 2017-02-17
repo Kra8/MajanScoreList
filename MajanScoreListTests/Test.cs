b7216259a6a8dcd27cb4ab8a29a0878e5793fea6
Koki Asai <kra.enmn0618.swift@softbank.ne.jp>
2017-02-17 11:39:48 +0900

    fix Test

--- MajanScoreListTests/Test.cs
+++ MajanScoreListTests/Test.cs
@@ -9,11 +9,11 @@ namespace MajanScoreListTests
 	{
 		[TestCase(30, 1, 1500)]
 		[TestCase(40, 4, 12000)]
-		public void TestScore(int hu, int han, int score)
+		public void TestScore(int hu, int han, int answer)
 		{
-			var _score = new Score(ScoreType.Parent, hu, han);
+			var score = new Score(ScoreType.Parent, hu, han);
 
-			Assert.True(_score.Ron == score);
+			Assert.True(score.Ron == answer);
 		}
 	}
 }
