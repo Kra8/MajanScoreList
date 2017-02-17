5f88079a715f0c1f9a0412b47cac84813b6ebf9b
Koki Asai <kra.enmn0618.swift@softbank.ne.jp>
2017-02-17 11:11:09 +0900

    Create Tests

---
+++ MajanScoreListTests/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using MajanScoreList;
+
+namespace MajanScoreListTests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[TestCase(30, 1, 1500)]
+		[TestCase(40, 4, 12000)]
+		public void TestScore(int hu, int han, int score)
+		{
+			var _score = new Score(ScoreType.Parent, hu, han);
+
+			Assert.True(_score.Ron == score);
+		}
+	}
+}
